1d0f4aa3783e64e00e24098e3ed521dccfdcd7c7
shitienc <shitiencheung@gmail.com>
2023-05-19 15:15:31 +0200

    Tilfølelse af shop og nye billeder

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// JAVASCRIPT TIL SLIDE UD TIL NAVIGATIONSBAR | Kilde fra: https://www.youtube.com/watch?v=oYRda7UtuhA&t=3245s //
+
 var navLinks = document.getElementById("navLinks");
 
 function showMenu(){
@@ -7,3 +9,12 @@ function showMenu(){
 function hideMenu(){
     navLinks.style.left = "-100%";
 }
+
+// JAVASCRIPT TIL SCROLL HORIZONT | Kilde fra: https://alvarotrigo.com/blog/scroll-horizontally-with-mouse-wheel-vanilla-java/ //
+
+const scrollContainer = document.querySelector(".wrapper");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    scrollContainer.scrollLeft += evt.deltaY;
+});
